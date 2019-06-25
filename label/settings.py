859f63fbacec74be32c1e9b32908cb97f928de5f
flash <flash.imagintech7102@gmail.com>
2019-06-25 23:54:00 +0530

    fix: code-bug

--- label/settings.py
+++ label/settings.py
@@ -3,4 +3,5 @@ import os
 # __file__ refers to the file settings.py 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))   # refers to application_top
 APP_DATASET = os.path.join(APP_ROOT, 'static/dataset')
-DATABASE_URL = os.environ.get("DATABASE_URL", default=None)
+# DATABASE_URL = os.environ.get("DATABASE_URL", default=None)
+DATABASE_URL = "mongodb://localhost:27017/labell_db"
